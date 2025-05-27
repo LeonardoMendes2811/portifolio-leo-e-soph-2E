8e7f7f0b84c0e3961441bde4501e48b8fee6757a
LeonardoMendes2811 <142437970+LeonardoMendes2811@users.noreply.github.com>
2025-05-27 12:25:15 -0300

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,17 +1,17 @@
-var nota1 = Number(window.prompt("digite sua nota"))
-var nota2 = Number(window.prompt("digite sua nota2"))
-var nota3 = Number(window.prompt("digite sua nota3"))
+let nome = Number(window.prompt("digite seu nome: "))
+let nota1 = Number(window.prompt("digite sua nota"+ nome +":"))
+let nota2 = Number(window.prompt("digite sua nota2"+ nome +":"))
+let nota3 = Number(window.prompt("digite sua nota3"+ nome +":"))
+let pontosFaltando;
 
-let(nota1 + nota2 + nota3)/ 3;
-
+let total = (nota1 + nota2 + nota3)/ 3;
+alura = "alura"
 alert("media final:" + total )
-
-if(total <=60){
-    alert("vc atingiu a media" total)
-
+if(total >=60){
+    alert("vc atingiu a media" + total )
 }
 else{
-    pontosFaltando = 60 - total
-    alert("reprovado faltaram" +pontosFinais + "pontos")
-
+    pontosFaltando = 60 - total;
+    alert(nome + "reprovado faltaram" + pontosFaltando + "pontos, estudar no alura")
 }
+
