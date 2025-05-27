93fce819c24895386adac016241bd368baf2f73f
LeonardoMendes2811 <142437970+LeonardoMendes2811@users.noreply.github.com>
2025-05-27 08:50:01 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,17 @@
+var nota1 = Number(window.prompt("digite sua nota"))
+var nota2 = Number(window.prompt("digite sua nota2"))
+var nota3 = Number(window.prompt("digite sua nota3"))
+
+let(nota1 + nota2 + nota3)/ 3;
+
+alert("media final:" + total )
+
+if(total <=60){
+    alert("vc atingiu a media" total)
+
+}
+else{
+    pontosFaltando = 60 - total
+    alert("reprovado faltaram" +pontosFinais + "pontos")
+
+}
